098c45d5e84a79c53a6731a98ca3f26d95bc3ab8
unknown <emiderex112@gmail.com>
2023-11-18 15:38:25 +0100

    Add Javascript files

---
+++ Javascript/background.js
@@ -0,0 +1,21 @@
+//const button = document.querySelector("button")
+//const body = document.querySelector("body")
+
+//const color = ["black", "red", "yellow"]
+
+//body.style.backgroundColor = "violet"
+
+//button.addEventListener ('click, changeB')
+
+//function changeB(){
+  //const colorIndex = parseInt(Math.random()*color.length)
+  //body.style.backgroundColor = color[colorIndex]
+//}
+
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+
+toggle.onclick = function(){
+toggle.classList.toggle('active');
+body.classList.toggle('active');
+}
